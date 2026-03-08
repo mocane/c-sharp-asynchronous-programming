cb2dd9f1e167f361c2d43041b695d3258251aae5
Emil Mocan <emil.mocan@protonmail.ch>
2026-03-08 13:57:36 +0200

    Keep only cross platform

--- src/Windows/08/Completed/StockAnalyzer.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StockAnalyzer.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
